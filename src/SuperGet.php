27442ac406decc4fe2a4cfd747eadba4d2a98f39
root <3320376695@qq.com>
2017-01-16 17:40:23 +0800

    add cache configs

--- src/SuperGet.php
+++ src/SuperGet.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
 
 namespace SuperGet;
 
 class SuperGet
 {
 
-   public static function info ()
-   {
-      echo 'u get info ';
-   }
+    public static function info ()
+    {
+        echo 'u get info ';
+    }
+
+    /**
+     * @param  array  $configs
+     * @return void
+     */
+    public static function setConfig($configs = [])
+    {
+//        SConfig::import($configs);TODO
+    }
 
 }
